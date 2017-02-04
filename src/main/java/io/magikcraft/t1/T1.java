ff97ac9d7895c54e3aa4efc8485e7ad8391a9740
Kyle Lewer (@kylelt) <kylelt@noreply.github.com>
2017-02-04 15:46:12 +1000

    added scheduler, need to confirm what delay and period units are for adequate timings

--- src/main/java/io/magikcraft/t1/T1.java
+++ src/main/java/io/magikcraft/t1/T1.java
@@ -3,24 +3,35 @@ package io.magikcraft.t1;
 import io.magikcraft.t1.command.EatCommandExecutor;
 import io.magikcraft.t1.command.TakeCommandExecutor;
 import io.magikcraft.t1.state.T1StateManager;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
-/**
- * Hello world!
- *
- */
+
 public class T1 extends JavaPlugin {
     private T1StateManager stateManager;
-
+    private BukkitRunnable t1StateUpdateScheduler;
     @Override
     public void onEnable() {
         stateManager = new T1StateManager(null);
 
         getCommand("eat").setExecutor(new EatCommandExecutor(stateManager));
         getCommand("take").setExecutor(new TakeCommandExecutor(stateManager));
+
+        t1StateUpdateScheduler = new BukkitRunnable() {
+            @Override
+            public void run() {
+                stateManager.update();
+            }
+        };
+        
+        t1StateUpdateScheduler.runTaskTimer(this,1, 300);
     }
 
     @Override
-    public void onDisable(){}
+    public void onDisable(){
+        t1StateUpdateScheduler.cancel();
+    }
 
 }
